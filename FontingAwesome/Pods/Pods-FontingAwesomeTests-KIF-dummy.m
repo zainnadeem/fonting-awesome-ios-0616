7b0b8c516d4284488f6a85fa800339feb68abed2
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-09-03 19:02:32 -0400

    add pods

---
+++ FontingAwesome/Pods/Pods-FontingAwesomeTests-KIF-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FontingAwesomeTests_KIF : NSObject
+@end
+@implementation PodsDummy_Pods_FontingAwesomeTests_KIF
+@end
---
+++ FontingAwesome/Pods/Pods-FontingAwesomeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FontingAwesomeTests : NSObject
+@end
+@implementation PodsDummy_Pods_FontingAwesomeTests
+@end
