edf5efd30c205f9b93c1ee5765e1abf09a682726
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-09-03 18:57:48 -0400

    add project

---
+++ FontingAwesome/FontingAwesome/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FontingAwesome
+//
+//  Created by Chris Gonzales on 9/3/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FontingAwesome/FontingAwesome/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FontingAwesome
+//
+//  Created by Chris Gonzales on 9/3/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
